5aa9d86ca4e6c8a806e00802d409406b3f0fdfd7
Sam <sam@mousa.nl>
2015-09-21 11:59:46 +0200

    Dev Don't compile when no changes.

--- protected/views/menu.php
+++ protected/views/menu.php
@@ -2,6 +2,7 @@
     use yii\bootstrap\Nav;
     use yii\bootstrap\NavBar;
     use yii\widgets\Menu;
+    use kartik\helpers\Html;
     NavBar::begin([
         'brandLabel' => \kartik\helpers\Html::img('@web/img/logo.svg'),
         'brandUrl' => Yii::$app->homeUrl,
@@ -10,23 +11,42 @@
         ],
         'renderInnerContainer' => false
     ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav'],
-        'items' => [
-            ['label' => 'Tools', 'url' => ['users/login']],
-            ['label' => 'Projects', 'url' => ['users/login']],
-            ['label' => 'Marketplace', 'url' => ['users/login']]
-        ],
-    ]);
+echo Nav::widget([
+    'options' => ['class' => 'navbar-nav'],
+    'items' => [
+        ['label' => 'Tools', 'url' => ['users/login']],
+        ['label' => 'Projects', 'url' => ['users/login']],
+        ['label' => 'Marketplace', 'url' => ['users/login']]
+    ],
+]);
 
 echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            Yii::$app->user->isGuest ?
-                ['label' => 'Login', 'url' => ['users/login']] :
-                ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                    'url' => ['/site/logout'],
-                    'linkOptions' => ['data-method' => 'post']],
+    'options' => ['class' => 'navbar-nav navbar-right'],
+    'encodeLabels' => false,
+    'items' => [
+        [
+            'label' => Html::icon('search'),
+            'url' => ['users/account'],
+            'visible' => Yii::$app->user->isGuest
+
         ],
-    ]);
-    NavBar::end();
+
+        [
+            'label' => Html::icon('user'),
+            'url' => ['users/account'],
+            'visible' => Yii::$app->user->isGuest
+
+        ],
+
+
+        Yii::$app->user->isGuest ?
+            [
+                'label' => Html::icon('log-in'),
+                'url' => ['users/login']
+            ] :
+            ['label' => Html::icon('log-out'),
+                'url' => ['/site/logout'],
+                'linkOptions' => ['data-method' => 'post']],
+    ],
+]);
+NavBar::end();
