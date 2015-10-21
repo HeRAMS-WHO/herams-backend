cca477d50ffa4019858a5bf27cc98df6b9b1dc14
Joey Claessen <joey_claessen@hotmail.com>
2015-10-21 14:53:46 +0000

    fixed console dev config include

--- protected/config/console.php
+++ protected/config/console.php
@@ -10,7 +10,7 @@ if (YII_DEBUG && file_exists(__DIR__ . '/debug.php')) {
     $config = \yii\helpers\ArrayHelper::merge($config, include(__DIR__ . '/debug.php'));
 }
 if (defined('YII_ENV') && file_exists(__DIR__ . '/' . YII_ENV . '.php')) {
-    $config = \yii\helpers\ArrayHelper::merge($config, include(__DIR__ . '/' . YII_ENV . '.php'));
+    $config = \yii\helpers\ArrayHelper::merge($config, include(__DIR__ . '/envs/' . YII_ENV . '.php'));
 }
 if (file_exists(__DIR__ . '/local.php')) {
     $config = yii\helpers\ArrayHelper::merge($config, include(__DIR__ . '/local.php'));
