5c73d5961412743266e32f6fbf36858cf5e65355
Sam Mousa <sam@mousa.nl>
2015-10-21 16:34:23 +0200

    Errorenous dev.php commit.

--- protected/config/envs/dev.php
+++ protected/config/envs/dev.php
@@ -5,3 +5,22 @@ return [
         'gii' => yii\gii\Module::class
     ]
 ];
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => \yii\db\Connection::class,
+            'charset' => 'utf8',
+            'dsn' => 'mysql:host=localhost;dbname=who;',
+            'password' => 'z2P6NUSj3YfcfVH4',
+            'username' => 'who',
+            'enableSchemaCache' => true,
+            'schemaCache' => 'cache',
+            'enableQueryCache' => true,
+            'queryCache' => 'cache',
+			'tablePrefix' => 'prime2_'
+        ]
+    ]
+];
+
+
