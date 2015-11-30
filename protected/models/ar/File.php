498dddb9e00d2e41856c52f34117b484982058ce
Joey Claessen <joey_claessen@hotmail.com>
2015-11-30 14:14:47 +0000

    added file

---
+++ protected/models/ar/File.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace prime\models\ar;
+
+use prime\components\ActiveRecord;
+use yii\validators\RequiredValidator;
+use yii\validators\StringValidator;
+
+class File extends ActiveRecord
+{
+    public function rules()
+    {
+        return [
+            [['mime_type', 'data'], RequiredValidator::class],
+            [['name', 'mime_type'], StringValidator::class]
+        ];
+    }
+}
