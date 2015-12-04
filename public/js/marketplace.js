64c03da0791c3f0fcb30ef54439cd0e8e8bfc7ef
Joey Claessen <joey_claessen@hotmail.com>
2015-12-04 10:34:06 +0000

    open marketplace summary in new page:

--- public/js/marketplace.js
+++ public/js/marketplace.js
@@ -5,8 +5,22 @@ function select(point, layer) {
         data: {id: id, layer: layer}
     })
     .success(function(body) {
-        bootbox.alert({
-            message: body
+        bootbox.dialog({
+            message: body,
+            buttons: [
+                {
+                    label: '<span class="glyphicon glyphicon-new-window"></span>',
+                    className: "btn-default",
+                    callback: function() {
+                        window.open('/marketplace/summary?layer=' + layer + '&id=' + id);
+                    }
+                },
+                {
+                    label: '<span class="glyphicon glyphicon-ok"></span>',
+                    className: "btn-primary",
+
+                }
+            ]
         });
     });
 }
