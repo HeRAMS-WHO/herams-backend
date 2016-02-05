9cc7734ada35e1897a0d0e2dd5bba5767b35ff6a
Sam Mousa <sam@mousa.nl>
2016-02-05 18:12:35 +0100

    Created config.

--- protected/config/envs/prod.php
+++ protected/config/envs/prod.php
@@ -1,5 +1,21 @@
 <?php
-
 return [
-
+    'components' => [
+        'db' => [
+            'class' => \yii\db\Connection::class,
+            'charset' => 'utf8',
+            'dsn' => 'mysql:host=localhost;dbname=who;',
+            'password' => 'z2P6NUSj3YfcfVH4',
+            'username' => 'who',
+            'enableSchemaCache' => true,
+            'schemaCache' => 'cache',
+            'enableQueryCache' => true,
+            'queryCache' => 'cache',
+            'tablePrefix' => 'prime2_',
+            'on afterOpen' => function($event) {
+                $event->sender->createCommand("SET SESSION sql_mode = strict_trans_tables;")->execute();
+            }
+        ]
+    ]
 ];
+
