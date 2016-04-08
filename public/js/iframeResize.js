febc99524c3b63054fa977cd582211236319d2cc
Sam Mousa <sam@mousa.nl>
2016-04-08 16:41:09 +0200

    Fixed issue #P-128 (Fixed) Added shiny link.

--- public/js/iframeResize.js
+++ public/js/iframeResize.js
@@ -1,13 +1,31 @@
 $(function(){
-    var $iframe = $("iframe.resize");
-    var resizer = function(e) {
-        $iframe.height($iframe.contents().find("body").height());
+
+    var resizer = function($iframe) {
+        console.log($iframe.contents().find("body").height());
+        console.log($iframe.contents().find("body").width());
+        // debugger;
+        if ($iframe.attr('data-resize') === 'height') {
+            $iframe.height($iframe.contents().find("body").height());
+            return;
+        } 
         $iframe.width($iframe.contents().find("body").width());
     };
 
-    $iframe.on("load", function() {
-        var $body = $iframe.contents().find("body");
-        $body.on("mresize", resizer);
+    $("iframe.resize").on("load", function() {
+        console.log("Watching iframe.");
+        var $body = $(this).contents().find("body");
+        var $this = $(this);
+        $body.on("mresize", function() {
+            resizer($this);
+        });
         $body.trigger("mresize");
+    }).each(function(i, el) {
+        setTimeout(function() {
+            resizer($(el));
+        }, 500);
     });
+
+
+
+
 });
