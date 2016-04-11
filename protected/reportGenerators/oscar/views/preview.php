2f22493d58946ed45160b4327a9cabcc1d01d03f
Sam Mousa <sam@mousa.nl>
2016-04-11 10:56:32 +0200

    Fixed errors in images in reports

--- protected/reportGenerators/oscar/views/preview.php
+++ protected/reportGenerators/oscar/views/preview.php
@@ -435,7 +435,9 @@ $until = (new \Carbon\Carbon($generator->getQuestionValue('gi3')))->format($gene
                     <td><?=\Yii::t('oscar', 'WHO')?></td>
                     <td><?=$formatter->asInteger($generator->getQuestionValue('resmob2[rmwho_SQ001]'))?></td>
                     <td><?=$formatter->asInteger($generator->getQuestionValue('resmob2[rmwho_SQ002]'))?></td>
-                    <td><?=$formatter->asPercent($generator->getQuestionValue('resmob2[rmwho_SQ002]') / $generator->getQuestionValue('resmob2[rmwho_SQ001]'))?></td>
+                    <td><?=$formatter->asPercent(
+                            $generator->getPercentage($generator->response, 'resmob2[rmwho_SQ002]', 'resmob2[rmwho_SQ001]')
+                        )?></td>
                 </tr>
                 <tr>
                     <td><?=\Yii::t('oscar', 'Health sector')?></td>
