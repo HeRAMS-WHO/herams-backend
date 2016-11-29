51cad548a97cff3558f3f3ac24d6c5a5bac23f7f
Sam Mousa <sam@mousa.nl>
2016-11-29 16:22:02 +0100

    Removed call to codecept_debug

--- protected/tests/_bootstrap.php
+++ protected/tests/_bootstrap.php
@@ -6,7 +6,7 @@ define('YII_ENV', getenv('YII_ENV'));
 // This is global bootstrap for autoloading
 $loader = require __DIR__ . '/../vendor/autoload.php';
 
-codecept_debug('Creating combined sql file:');
+//codecept_debug('Creating combined sql file:');
 
 $base = __DIR__;
 if (!is_dir($base . '/_output')) {
