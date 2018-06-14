2611cfba4a8274028313e00e59de3a6466345d09
markusnovelt <mv@novel-t.ch>
2018-06-14 09:49:27 +0200

    Fixed merge conflict

--- protected/controllers/ToolsController.php
+++ protected/controllers/ToolsController.php
@@ -376,15 +376,4 @@ class ToolsController extends Controller
             ]
         );
     }
-
-    public function actionOverview(
-        int $id
-    ) {
-        $tool = Tool::loadOne($id);
-        $this->layout = 'angular';
-
-        return $this->render('overview', [
-            'model' => $tool,
-        ]);
-    }
 }
