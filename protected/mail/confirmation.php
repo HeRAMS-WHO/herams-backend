5348f27e8112adebb70c7be48e5d5511c808ac5a
Sam Mousa <sam@mousa.nl>
2018-11-27 10:42:58 +0100

    Fix syntax

--- protected/mail/confirmation.php
+++ protected/mail/confirmation.php
@@ -25,6 +25,7 @@ use yii\helpers\Html;
 </p>
 <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
     <?= Html::a(Html::encode(app()->urlManager->createAbsoluteUrl(['/user/login'])), app()->urlManager->createAbsoluteUrl(['/user/login'])); ?>
+    <?= Html::a(Html::encode($token->url), $token->url) ?>
 </p>
 <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
     <?= Yii::t('user', 'If you cannot click the link, please try pasting the text into your browser') ?>.
