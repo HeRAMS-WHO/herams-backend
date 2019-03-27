dabbf3602530c982cc8ffbc546a021e596ed0aa9
Sam Mousa <sam@mousa.nl>
2019-03-27 14:32:36 +0100

    Use post method

--- protected/views/page/create.php
+++ protected/views/page/create.php
@@ -30,7 +30,6 @@ $this->params['breadcrumbs'][] = $this->title;
 
     $form = ActiveForm::begin([
         'id' => 'update-page',
-        'method' => 'PUT',
         "type" => ActiveForm::TYPE_HORIZONTAL,
     ]);
 
