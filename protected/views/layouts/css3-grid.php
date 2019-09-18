47f192da59549c64bcd56b922763616af60d5e41
Sam Mousa <sam@mousa.nl>
2019-09-18 15:53:49 +0200

    Possible fix for #218

--- protected/views/layouts/css3-grid.php
+++ protected/views/layouts/css3-grid.php
@@ -17,11 +17,6 @@ use yii\widgets\Breadcrumbs;
 <head>
     <?= Html::csrfMetaTags() ?>
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <script src="https://browser.sentry-cdn.com/5.6.3/bundle.min.js" integrity="sha384-/Cqa/8kaWn7emdqIBLk3AkFMAHBk0LObErtMhO+hr52CntkaurEnihPmqYj3uJho" crossorigin="anonymous">
-    </script>
-    <script>
-        Sentry.init({ dsn: 'https://b309b0529f0d45cc86b019644b8e2e43@sentry.io/1730691' });
-    </script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU=" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.5.2/flatpickr.min.css" integrity="sha256-TV6wP5ef/UY4bNFdA1h2i8ASc9HHcnl8ufwk94/HP4M=" crossorigin="anonymous" />
