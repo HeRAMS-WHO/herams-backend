990f029de37f16baabea1d394a30e3ce9a186d8c
jérémie helme <jeremiehelme@gmail.com>
2019-12-16 10:46:05 +0100

    css for popovers and menus

--- protected/views/user-menu.php
+++ protected/views/user-menu.php
@@ -5,19 +5,18 @@ use yii\helpers\Html;
 
 ?>
 <div class="user-menu">
-    <?= Html::a(Icon::signOutAlt(), ['/session/delete'], ['data-method' => 'delete']); ?>
     <?php
     /** @var \prime\models\ar\User $user */
     $user = \Yii::$app->user->identity;
     ?>
 
-    <div>
+    <div class="username">
         <?= Html::a($user->name, ['/user/account'], [
             'class' => 'name'
         ]); ?>
         <div class="email"><?= $user->email ?></div>
     </div>
-    <?php
-        echo Html::a(Icon::admin(), ['/admin/dashboard']);
-    ?>
+    <?= Html::a(Icon::admin(), ['/admin/dashboard']); ?>
+    <?= Html::a(Icon::signOutAlt(), ['/session/delete'], ['data-method' => 'delete']); ?>
+
 </div>
