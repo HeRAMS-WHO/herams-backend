733aba55a2cf97468fdfe033bd9accf2d3981f24
jérémie helme <jeremiehelme@gmail.com>
2020-03-19 14:21:54 +0100

    displaying all the charts for printing

--- protected/views/project/print.php
+++ protected/views/project/print.php
@@ -40,30 +40,30 @@ $this->title = $project->getDisplayField();
 </div>
 <?php
 echo Html::beginTag('div', ['class' => 'content']);
-
-foreach ($page->getChildElements() as $element) {
-    Yii::beginProfile('Render element ' . $element->id);
-    echo "<!-- Begin chart {$element->id} -->";
-    $level = ob_get_level();
-    ob_start();
-    try {
-        echo $element->getWidget($survey, $data, $page)->run();
-        echo ob_get_clean();
-    } catch (Throwable $t) {
-        while (ob_get_level() > $level) {
-            ob_end_clean();
+foreach($project->pages as $page) {
+    foreach ($page->getChildElements() as $element) {
+        Yii::beginProfile('Render element ' . $element->id);
+        echo "<!-- Begin chart {$element->id} -->";
+        $level = ob_get_level();
+        ob_start();
+        try {
+            echo $element->getWidget($survey, $data, $page)->run();
+            echo ob_get_clean();
+        } catch (Throwable $t) {
+            while (ob_get_level() > $level) {
+                ob_end_clean();
+            }
+            \Yii::error($t);
+            echo Html::tag(
+                'div',
+                "Rendering this element caused an error: <strong>{$t->getMessage()}</strong>. The most common reason for the error is an invalid question code in its configuration. You can edit the element " . Html::a('here', ['/element/update', 'id' => $element->id]) . '.',
+                [
+                    'class' => 'element',
+                ]
+            );
         }
-        \Yii::error($t);
-        echo Html::tag(
-            'div',
-            "Rendering this element caused an error: <strong>{$t->getMessage()}</strong>. The most common reason for the error is an invalid question code in its configuration. You can edit the element " . Html::a('here', ['/element/update', 'id' => $element->id]) . '.',
-            [
-                'class' => 'element',
-            ]
-        );
+        echo "<!-- End chart {$element->id} -->";
+        Yii::endProfile('Render element ' . $element->id);
     }
-    echo "<!-- End chart {$element->id} -->";
-    Yii::endProfile('Render element ' . $element->id);
 }
-
 echo Html::endTag('div');
