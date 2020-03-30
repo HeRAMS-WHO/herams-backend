f38c1d2490d21a84ea032ed2146085faaec718ba
Sam Mousa <sam@mousa.nl>
2020-03-30 12:04:09 +0200

    Fixed large xlsx export

--- protected/helpers/XlsxWriter.php
+++ protected/helpers/XlsxWriter.php
@@ -3,25 +3,24 @@ declare(strict_types=1);
 
 namespace prime\helpers;
 
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Box\Spout\Writer\XLSX\Writer;
 use GuzzleHttp\Psr7\LazyOpenStream;
-use GuzzleHttp\Psr7\Stream;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use prime\interfaces\ColumnDefinition;
 use prime\interfaces\HeramsResponseInterface;
 use prime\interfaces\WriterInterface;
 use Psr\Http\Message\StreamInterface;
 use function GuzzleHttp\Psr7\stream_for;
 use function iter\map;
-use function iter\toArray;
 
 class XlsxWriter implements WriterInterface
 {
     /**
-     * @var Spreadsheet
+     * @var Writer
      */
-    private $file;
-    private $rows;
+    private $writer;
+
+    private $filename;
 
     public function __construct()
     {
@@ -31,20 +30,22 @@ class XlsxWriter implements WriterInterface
 
     private function reset()
     {
-        $this->file = new Spreadsheet();
-        $this->rows = 1;
+        ini_set('max_execution_time', '0');
+        // Set up the cache
+        $this->writer = WriterEntityFactory::createXLSXWriter();
+        $this->stream = stream_for('');
+        $this->filename = tempnam(sys_get_temp_dir(), 'xslx');
+        $this->writer->openToFile($this->filename);
     }
 
 
     private function writeRow(iterable $data): void
     {
-        $sheet = $this->file->getActiveSheet();
-        $column = 0;
-        foreach ($data as $value) {
-            $sheet->setCellValueByColumnAndRow($column, $this->rows, $value);
-            $column++;
+        $row = WriterEntityFactory::createRow([]);
+        foreach($data as $value) {
+            $row->addCell(WriterEntityFactory::createCell($value));
         }
-        $this->rows++;
+        $this->writer->addRow($row);
     }
 
     public function writeRecord(HeramsResponseInterface $record, ColumnDefinition ...$columns): void
@@ -70,11 +71,8 @@ class XlsxWriter implements WriterInterface
 
     public function getStream(): StreamInterface
     {
-        $writer = new Xlsx($this->file);
-        $tmpfile = tempnam(sys_get_temp_dir(), 'xlsx');
-        $writer->save($tmpfile);
-        $this->reset();
-        return new LazyOpenStream($tmpfile, 'r');
+        $this->writer->close();
+        return new LazyOpenStream($this->filename, 'r');
     }
 
     public function getMimeType(): string
