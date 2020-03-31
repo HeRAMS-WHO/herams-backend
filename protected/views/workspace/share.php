1731f5a38a93bfd442443264458c07560c4e1610
Sam Mousa <sam@mousa.nl>
2020-03-31 12:24:55 +0200

    Fixed share button in workspace/share view, removed wrapping div

--- protected/views/workspace/share.php
+++ protected/views/workspace/share.php
@@ -1,6 +1,8 @@
 <?php
 
 use app\components\ActiveForm;
+use app\components\Form;
+use prime\widgets\FormButtonsWidget;
 use yii\helpers\Html;
 
 /**
@@ -42,9 +44,19 @@ $this->params['breadcrumbs'][] = $this->title;
         ]);
 
         echo $model->renderForm($form);
+        echo Form::widget([
+            'form' => $form,
+            'model' => $model,
+            'attributes' => [
+                FormButtonsWidget::embed([
+                    'buttons' => [
+                        ['label' => \Yii::t('app', 'Add'), 'options' => ['class' => ['btn', 'btn-primary']]]
+                    ]
+                ])
+            ]
+        ]);
         $form->end();
         ?>
-        <button type="submit" class="btn btn-primary">Share</button>
     </div>
     <div class="col-xs-12 list-shared">
         <h2><?=\Yii::t('app', 'Already shared with')?></h2>
