1c14d59093e700c27633f2510f73b6cd67357102
jérémie helme <jeremiehelme@gmail.com>
2020-06-12 16:00:51 +0200

    changing layout for user popover

--- protected/views/layouts/map-popover.php
+++ protected/views/layouts/map-popover.php
@@ -12,16 +12,15 @@ $this->beginContent('@views/layouts/map.php');
 <div class="popover">
     <a href="/" style="position: absolute; right: 10px; top: 10px;"><?= Icon::close(); ?></a>
     <div class="intro">
-        <?php
-            echo Html::img('@web/img/HeRAMS.png');
-            echo Html::tag('p', \Yii::t('app', "The Health Resources and Services Availability Monitoring System (HeRAMS) is a collaborative approach aimed at ensuring that core information on essential health resources and services is systematically shared and readily available to decision makers at country, regional and global levels"));
-        ?>
+        <div class='logo white'>
+           <object type="image/svg+xml" data="../img/HeRAMS_white.svg"></object>
+        </div>
+        <?php echo Html::tag('header', \Yii::t('app', 'The Health Resources and Services Availability Monitoring System (HeRAMS) is a collaborative approach aimed at ensuring that core information on essential health resources and services is systematically shared and readily available to decision makers at country, regional and global levels')); ?>
+        <?= $this->render('stats'); ?>
     </div>
     <div class="form">
         <?=$content; ?>
     </div>
-    <?=$this->render('stats'); ?>
-
 </div>
 <?php
 
