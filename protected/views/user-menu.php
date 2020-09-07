5bbe2d921d128bf0ae4a8550526a2a88eb6b683d
jérémie helme <jeremiehelme@gmail.com>
2020-09-07 09:58:30 +0200

    adding language code to user menu

--- protected/views/user-menu.php
+++ protected/views/user-menu.php
@@ -16,14 +16,15 @@ use yii\helpers\Url;
         echo Html::tag('span', "DEBUG CURRENT LANGUAGE: " . \Yii::$app->language);
     }
 
-        $lang = \Yii::$app->language;
-        if(strpos($lang,'-') !== -1)
-            $lang = explode('-',$lang)[0];
-        echo Html::a(Icon::star(), ['/user/favorites']);
-        echo Html::a(Icon::admin(), ['/admin/dashboard']);
-        echo Html::a(Icon::question(), Url::to('https://docs.herams.org/'), ['target'=> '_blank']);
-        echo Html::a(strtoupper($lang), ['/user/account']);
-        echo Html::a(Icon::signOutAlt(), ['/session/delete'], ['data-method' => 'delete']);
+    $lang = \Yii::$app->language;
+    if (strpos($lang, '-') !== -1) {
+        $lang = explode('-', $lang)[0];
+    }
+    echo Html::a(Icon::star(), ['/user/favorites']);
+    echo Html::a(Icon::admin(), ['/admin/dashboard']);
+    echo Html::a(Icon::question(), Url::to('https://docs.herams.org/'), ['target'=> '_blank']);
+    echo Html::a(strtoupper($lang), ['/user/account']);
+    echo Html::a(Icon::signOutAlt(), ['/session/delete'], ['data-method' => 'delete']);
     ?>
 
     <div>
