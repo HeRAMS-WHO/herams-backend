8fbd59d75da7ece7e554b68cde8f0623725e9590
jérémie helme <jeremiehelme@gmail.com>
2020-10-07 10:32:23 +0200

    hiding language icon if null

--- protected/views/user-menu.php
+++ protected/views/user-menu.php
@@ -15,14 +15,16 @@ use yii\helpers\Url;
     if (YII_DEBUG) {
         echo Html::tag('span', "DEBUG CURRENT LANGUAGE: " . \Yii::$app->language);
     }
-    $lang = \Yii::$app->request->getPreferredLanguage(\Yii::$app->params['languages']);
-    if (strpos($lang, '-') !== -1) {
+    $lang = \Yii::$app->language;
+    if (strpos($lang, '-')) {
         $lang = explode('-', $lang)[0];
     }
     echo Html::a(Icon::star(), ['/user/favorites']);
     echo Html::a(Icon::admin(), ['/admin/dashboard']);
     echo Html::a(Icon::question(), Url::to('https://docs.herams.org/'), ['target'=> '_blank']);
-    echo Html::a(strtoupper($lang), ['/user/account']);
+    if (!empty($lang)) {
+        echo Html::a(strtoupper($lang), ['/user/account']);
+    }
     echo Html::a(Icon::signOutAlt(), ['/session/delete'], ['data-method' => 'delete']);
     ?>
 
