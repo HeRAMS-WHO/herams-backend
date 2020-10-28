8959ed6553d2c97d8c0b746e167187fe49fd7f28
Sam Mousa <sam@mousa.nl>
2020-10-28 09:46:23 +0100

    composer 2 compatibility

--- docker/cli/init.sh
+++ docker/cli/init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 wait4ports devdb=tcp://devdb:3306 &&
 #/project/protected/yiic cache/warmup &&
-exec php -S 0.0.0.0:8080 -t /project/public
+exec php -d memory_limit=2g -S 0.0.0.0:8080 -t /project/public
