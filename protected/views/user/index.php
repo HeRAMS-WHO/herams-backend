fd3764ba44c24648bd7a0dc2de51e598a417fd87
Sam Mousa <sam@mousa.nl>
2021-02-01 11:30:55 +0100

    fix console k8s detection

--- protected/views/user/index.php
+++ protected/views/user/index.php
@@ -1,13 +1,5 @@
 <?php
-
-/*
- * This file is part of the Dektrium project.
- *
- * (c) Dektrium project <http://github.com/dektrium>
- *
- * For the full copyright and license information, please view the LICENSE.md
- * file that was distributed with this source code.
- */
+declare(strict_types=1);
 
 use kartik\grid\ActionColumn;
 use kartik\grid\GridView;
@@ -22,7 +14,7 @@ use yii\web\View;
 /**
  * @var View $this
  * @var ActiveDataProvider $dataProvider
- * @var UserSearch $searchModel
+ * @var \prime\models\search\User $searchModel
  */
 $this->title = \Yii::t('app', 'Administration');
 
--- protected/yiic.php
+++ protected/yiic.php
@@ -11,12 +11,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 spl_autoload_register(['Yii', 'autoload'], true, true);
 
 // Detect if we are in K8s...
-if (!YII_ENV_PROD) {
-    $env = new \prime\components\InsecureSecretEnvironment(__DIR__ . '/config/env.json');
+if (!file_exists('/run/secrets')) {
+    $env = new \prime\components\InsecureSecretEnvironment('/run/env.json', __DIR__ . '/config/env.json');
 } else {
     $env = new KubernetesSecretEnvironment('/run/secrets', __DIR__ . '/config/env.json', '/run/config/config.json', '/run/env.json');
 }
-
 
 $config = require __DIR__ . '/config/console.php';
 \Yii::$container->set(\yii\console\Application::class, $config);
