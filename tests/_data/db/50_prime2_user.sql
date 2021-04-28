7bc5ab5dc7ac0935050cb16c42f1ada2e05a94f7
Joey Claessen <joey_claessen@hotmail.com>
2021-04-28 07:08:31 +0000

    Added test users to test database

--- tests/_data/db/50_prime2_user.sql
+++ tests/_data/db/50_prime2_user.sql
@@ -1,6 +1,8 @@
 SET FOREIGN_KEY_CHECKS=0;
 SET NAMES 'utf8';
-INSERT INTO `prime2_user` (`id`, `email`, `password_hash`, `created_at`, `updated_at`, `last_login_at`, `name`, `language`) VALUES (1, 'admin@user.com', 'INVALIDHASH', 1, 1, 1, 'admin', NULL);
-INSERT INTO `prime2_user` (`id`, `email`, `password_hash`, `created_at`, `updated_at`, `last_login_at`, `name`, `language`) VALUES (2, 'user@user.com', 'INVALIDHASH', 1, 1, 1, 'user', NULL);
-INSERT INTO `prime2_user` (`id`, `email`, `password_hash`, `created_at`, `updated_at`, `last_login_at`, `name`, `language`) VALUES (3, 'user2@user.com', 'INVALIDHASH', 1, 1, 1, 'user2', NULL);
+INSERT INTO `prime2_user` (`id`, `email`, `password_hash`, `created_at`, `updated_at`, `last_login_at`, `name`, `language`) VALUES (1, 'test-admin@user.com', 'INVALIDHASH', 1, 1, 1, 'test-admin', NULL);
+INSERT INTO `prime2_user` (`id`, `email`, `password_hash`, `created_at`, `updated_at`, `last_login_at`, `name`, `language`) VALUES (2, 'test-user@user.com', 'INVALIDHASH', 1, 1, 1, 'test-user', NULL);
+INSERT INTO `prime2_user` (`id`, `email`, `password_hash`, `created_at`, `updated_at`, `last_login_at`, `name`, `language`) VALUES (3, 'test-user2@user.com', 'INVALIDHASH', 1, 1, 1, 'test-user2', NULL);
+INSERT INTO `prime2_user` (`id`, `email`, `password_hash`, `created_at`, `updated_at`, `last_login_at`, `name`, `language`) VALUES (4, 'admin@user.com', '$2y$13$sPDjKPQhfkqfNGD1vkuJtuedaBHOJkTQJsf5DEG49TG.HU8T6Fwua', 1, 1, 1, 'Admin', NULL);
+INSERT INTO `prime2_user` (`id`, `email`, `password_hash`, `created_at`, `updated_at`, `last_login_at`, `name`, `language`) VALUES (5, 'user@user.com', '$2y$13$sPDjKPQhfkqfNGD1vkuJtuedaBHOJkTQJsf5DEG49TG.HU8T6Fwua', 1, 1, 1, 'User', NULL);
 SET FOREIGN_KEY_CHECKS=1;
