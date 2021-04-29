cfdb3b6ae7769078adafb941f81ca3043db1e911
Sam Mousa <sam@mousa.nl>
2021-04-29 15:26:33 +0200

    add facility migration

---
+++ protected/migrations/m210324_094516_create_facility_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%facility}}`.
+ */
+class m210324_094516_create_facility_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%facility}}', [
+            'id' => $this->primaryKey(),
+            'uuid' => ' binary(16) not null',
+            'name' => $this->string(),
+            'alternative_name' => $this->string(),
+        ]);
+        $this->createIndex('uuid', '{{%facility}}', ['uuid'], true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%facility}}');
+    }
+}
