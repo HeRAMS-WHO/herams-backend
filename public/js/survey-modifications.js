1f3a36342ca89a79d2057fdbb6eb0c44bdcfaa15
Sam Mousa <sam@mousa.nl>
2021-11-03 14:06:18 +0100

    add facilityType question type

--- public/js/survey-modifications.js
+++ public/js/survey-modifications.js
@@ -1,63 +1,105 @@
-/**
-Survey.Serializer.addProperty("text", {
+
+((Survey) => {
+    /**
+     Survey.Serializer.addProperty("text", {
     name: "isPostalCode",
     type: "switch",
     category: "Statistics & analytics",
 });
-*/
-[
-    "mode",
-    "navigateToUrl",
-    "navigateToUrlOnCondition",
-    "maxTimeToFinish",
-    "maxTimeToFinishPage",
-    "showTimerPanel",
-    "showTimerPanelMode",
-    "completedBeforeHtml",
-    "completedHtml",
-    "completedHtmlOnCondition",
-    "showCompletedPage",
-    "loadingHtml",
-    "storeOthersAsComment",
-    "sendResultOnPageNext",
-    "requiredText",
-    "questionStartIndex",
-    "logo",
-    "logoFit",
-    "logoHeight",
-    "logoPosition",
-    "logoWidth",
-    "locale",
-    "title",
-    "showTitle",
-    "description",
-    "cookieName",
-    "showPreviewBeforeComplete",
-    "pageNextText",
-    "pagePrevText",
-    "startSurveyText",
-    "requiredText",
-    "completeText",
-    "previewText",
-    "emptySurveyText",
-    "editText",
-    "firstPageIsStarted",
-    "progressText"
+     */
+    [
+        "mode",
+        "navigateToUrl",
+        "navigateToUrlOnCondition",
+        "maxTimeToFinish",
+        "maxTimeToFinishPage",
+        "showTimerPanel",
+        "showTimerPanelMode",
+        "completedBeforeHtml",
+        "completedHtml",
+        "completedHtmlOnCondition",
+        "showCompletedPage",
+        "loadingHtml",
+        "storeOthersAsComment",
+        "sendResultOnPageNext",
+        "requiredText",
+        "questionStartIndex",
+        "logo",
+        "logoFit",
+        "logoHeight",
+        "logoPosition",
+        "logoWidth",
+        "locale",
+        "title",
+        "showTitle",
+        "description",
+        "cookieName",
+        "showPreviewBeforeComplete",
+        "pageNextText",
+        "pagePrevText",
+        "startSurveyText",
+        "requiredText",
+        "completeText",
+        "previewText",
+        "emptySurveyText",
+        "editText",
+        "firstPageIsStarted",
+        "progressText"
 
 
+    ].forEach((property) => Survey.Serializer.removeProperty("survey", property));
+
+    Survey.Serializer.removeProperty("selectBase", "choicesByUrl");
+    Survey.QuestionFactory.Instance.unregisterElement("signaturepad");
+    Survey.QuestionFactory.Instance.unregisterElement("file");
+    Survey.QuestionFactory.Instance.unregisterElement("multipletext");
+    Survey.QuestionFactory.Instance.unregisterElement("paneldynamic");
+    Survey.QuestionFactory.Instance.unregisterElement("matrixdynamic");
+    Survey.QuestionFactory.Instance.unregisterElement("comment");
+    Survey.QuestionFactory.Instance.unregisterElement("expression");
+    /**
+     * Todo:
+     * sendResultOnPageNext --> should be forced to true
+     */
 
 
-].forEach((property) => Survey.Serializer.removeProperty("survey", property));
+// New question type for facility type:
+    const facilityQuestionType = {
+        name: "facilityType",
+        title: "Facility Type",
+        iconName: "icon-list",
+        category: "General",
+        isFit(question) {
+            return question.getType() === "facilityType";
+        },
+        isDefaultRender: true,
 
-Survey.Serializer.removeProperty("selectBase", "choicesByUrl");
-Survey.QuestionFactory.Instance.unregisterElement("signaturepad");
-Survey.QuestionFactory.Instance.unregisterElement("file");
-Survey.QuestionFactory.Instance.unregisterElement("multipletext");
-Survey.QuestionFactory.Instance.unregisterElement("paneldynamic");
-Survey.QuestionFactory.Instance.unregisterElement("matrixdynamic");
-Survey.QuestionFactory.Instance.unregisterElement("comment");
-Survey.QuestionFactory.Instance.unregisterElement("expression");
-/**
- * Todo:
- * sendResultOnPageNext --> should be forced to true
- */
+        widgetIsLoaded() {
+            return true
+        },
+
+        init() {
+            Survey.Serializer.addClass("facilityitemvalue", [
+                {
+                    name: "tier",
+                    choices: ["primary", "secondary", "tertiary"]
+                }, {
+                    name: "text"
+                }
+            ], null, "itemvalue");
+
+            Survey.Serializer.addClass("facilityType", [
+                {
+                    name: "choices",
+                    type: "facilityitemvalue[]"
+                },
+
+
+            ], null, "dropdown");
+
+        }
+    };
+
+    Survey.CustomWidgetCollection.Instance.add(facilityQuestionType, "customtype");
+
+})(Survey);
