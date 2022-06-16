5ac34d10071bc55221037c94b98fde2490bbb4e2
Sam Mousa <sam@mousa.nl>
2022-06-16 10:55:55 +0200

    fix: quick & dirty performance improvements

--- protected/controllers/site/WorldMap.php
+++ protected/controllers/site/WorldMap.php
@@ -12,7 +12,9 @@ class WorldMap extends Action
     public function run(User $user)
     {
         $this->controller->layout = 'css3-grid';
-        $dataProvider = (new \prime\models\search\Project())->search([], $user);
+        $search = new \prime\models\search\Project();
+        $search->withFields = [];
+        $dataProvider = $search->search([], $user);
         $dataProvider->setPagination(false);
         return $this->controller->render('world-map', [
             'projects' => $dataProvider
