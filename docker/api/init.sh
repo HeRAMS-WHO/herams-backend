681b55ca193ee3ccc1d64454b8be7e7818fcd5be
Sam Mousa <sam@mousa.nl>
2022-12-06 15:57:11 +0100

    fix: add api docker conf

---
+++ docker/api/init.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+cp -r /project/public/. /var/www/html
+mkdir -p /var/www/html/assets
+chown nobody:nobody /var/www/html/assets
+rm -rf /project/public/assets
+ln -s /var/www/html/assets /project/public/assets
+touch /run/env.json
+chown nobody:nobody /run/env.json
+env
+jq -s 'env+add' > /run/env.json
+
+# Check if we need to seed a database with SQL files.
+if [ -d "/database-seed" ]; then
+  echo "Seeding database";
+  cp -r /database/. /database-seed
+fi
+
+
+exec php-fpm --force-stderr --fpm-config /php-fpm.conf
