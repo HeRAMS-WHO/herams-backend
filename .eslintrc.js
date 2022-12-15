d62b55bafe06ab1b540c09d628305108f2407a2c
Sam Mousa <sam@mousa.nl>
2022-12-15 14:14:45 +0100

    chore: add some dev config files

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'standard',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+}
