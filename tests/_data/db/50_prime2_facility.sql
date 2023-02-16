2a99efc14f168bf6813e1dca1993206e5d131819
Sam Mousa <sam@mousa.nl>
2023-02-16 10:31:34 +0100

    chore: remove data from test set

--- tests/_data/db/50_prime2_facility.sql
+++
@@ -1,4 +0,0 @@
-SET FOREIGN_KEY_CHECKS=0;
-SET NAMES 'utf8';
-INSERT INTO `prime2_facility` (`id`, `workspace_id`, `code`, `data`, `admin_data`, `latitude`, `longitude`, `can_receive_situation_update`, `use_in_list`, `use_in_dashboarding`, `deleted_at`, `deactivated_at`, `latest_date`, `tier`) VALUES (1, 1, NULL, NULL, NULL, NULL, NULL, 1, 1, 1, NULL, NULL, NULL, NULL);
-SET FOREIGN_KEY_CHECKS=1;
--- tests/_data/db/50_prime2_workspace.sql
+++
@@ -1,4 +0,0 @@
-SET FOREIGN_KEY_CHECKS=0;
-SET NAMES 'utf8';
-INSERT INTO `prime2_workspace` (`id`, `i18n`, `project_id`, `closed_at`, `created_at`, `token`, `data`) VALUES (1, CAST('{\"title\":{\"en\":\"Workspace\"}}' AS JSON), 12, NULL, '2021-10-26 06:59:16', 'abcdefg', NULL);
-SET FOREIGN_KEY_CHECKS=1;
