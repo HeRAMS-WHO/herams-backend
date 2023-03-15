0a4e10f18624c1558da3482292d1a9e45b993e4f
v3795099 <cristian.girlea@pearson.com>
2023-03-15 17:18:06 +0200

    Updates for the PR.

--- microservices/api/src/components/Serializer.php
+++ microservices/api/src/components/Serializer.php
@@ -9,6 +9,10 @@ use herams\common\interfaces\LocalizableInterface;
 
 class Serializer extends \yii\rest\Serializer
 {
+    /**
+     * @param $data
+     * @return array|array[]|mixed|null
+     */
     public function serialize($data)
     {
         if ($data instanceof LocalizableInterface) {
--- microservices/api/src/controllers/ConfigurationController.php
+++ microservices/api/src/controllers/ConfigurationController.php
@@ -11,7 +11,7 @@ class ConfigurationController extends Controller
 {
     public function actionCountries(
         ConfigurationProvider $configurationProvider,
-        Response              $response): array
+        Response $response): array
     {
         $response->headers->add('Cache-Control', 'max-age=604800,public');
         return $configurationProvider->getPlatformCountries();
@@ -20,7 +20,7 @@ class ConfigurationController extends Controller
 
     public function actionLocales(
         ConfigurationProvider $configurationProvider,
-        Response              $response
+        Response $response
     ): array|string
     {
         $response->headers->add('Cache-Control', 'max-age=604800,public');
@@ -29,7 +29,7 @@ class ConfigurationController extends Controller
 
     public function actionVisibilities(
         ConfigurationProvider $configurationProvider,
-        Response              $response): array
+        Response $response): array
     {
         $response->headers->add('Cache-Control', 'max-age=604800,public');
         return $configurationProvider->getPlatformVisibilities();
