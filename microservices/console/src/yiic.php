5a28ee2276ce8b9514ad623a988fc79f70962209
Cristian Girlea <cristiangirlea@gmail.com>
2023-08-09 12:18:23 +0300

    update autoload path

--- microservices/console/src/yiic.php
+++ microservices/console/src/yiic.php
@@ -8,7 +8,7 @@ define('YII_ENV', getenv('YII_ENV'));
 defined('CONSOLE') or define('CONSOLE', true);
 
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', file_exists(__DIR__ . '/config/debug') ? 3 : 0);
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
 spl_autoload_register(['Yii', 'autoload'], true, true);
 
 error_reporting(E_ALL & ~E_DEPRECATED);
