d6d8548c3da3989b58b0c8f9310932b072670551
Cristian Girlea <cristiangirlea@gmail.com>
2023-08-18 18:57:15 +0300

    setup application version from version.txt and translator to retrieve the translations with the help of the extended getex.

---
+++ microservices/common/src/components/TranslationComponent.php
@@ -0,0 +1,38 @@
+<?php
+namespace herams\common\components;
+
+use herams\common\extensions\ExtendedGettextMessageSource;
+use Yii;
+use yii\base\Component;
+
+class TranslationComponent extends Component
+{
+    /**
+     * Get translations for a specific category and language.
+     *
+     * @param string $category
+     * @param string $language
+     * @return array
+     */
+    public function getTranslations($category = 'app', $language = null)
+    {
+        if ($language === null) {
+            $language = Yii::$app->language;
+        }
+
+        $i18n = Yii::$app->i18n;
+        $source = $i18n->getMessageSource('yii');
+
+        if ($source instanceof ExtendedGettextMessageSource) {
+            $messages = $source->publicLoadMessages($category, $language);
+            return $messages;
+        }
+        $sources = new \herams\common\extensions\ExtendedGettextMessageSource();
+
+
+        $messages = $source->publicLoadMessages($category, $language);
+
+        return $messages;
+    }
+}
+
