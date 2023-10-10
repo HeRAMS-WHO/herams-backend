f43e14ce1938df21754f291497e28a9b5d6a492f
Rafael Pérez <rafaelbenitoperezgonzalez@gmail.com>
2023-10-10 19:52:06 +0200

    json

--- protected/react-src/src/services/httpMethods.js
+++ protected/react-src/src/services/httpMethods.js
@@ -13,42 +13,17 @@ export const get = async (url, params = {}, headers = {}) => {
     return handleResponse(response);
 };
 
-const convertDataToFormUrlEncoded = (data, parentKey = '') => {
-    const form = new URLSearchParams();
-    for (const key in data) {
-        const newKey = parentKey ? `${parentKey}[${key}]` : key;
 
-        if (typeof data[key] === 'object') {
-            const nestedFormData = convertDataToFormUrlEncoded(data[key], newKey);
-            for (const [nestedKey, nestedValue] of nestedFormData) {
-                form.append(nestedKey, nestedValue);
-            }
-        } else if (Array.isArray(data[key])) {
-            for (let i = 0; i < data[key].length; i++) {
-                const nestedFormData = convertDataToFormUrlEncoded(
-                    { [i]: data[key][i] },
-                    `${newKey}`
-                );
-                for (const [nestedKey, nestedValue] of nestedFormData) {
-                    form.append(nestedKey, nestedValue);
-                }
-            }
-        } else {
-            form.append(newKey, data[key]);
-        }
-    }
-    return form;
-};
 export const post = async (url, data) => {
     try {
         const response = await fetch(url, {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+                'Content-Type': 'application/json',
                 'X-Csrf-Token': getCsrfToken(),
                 'X-Requested-With': 'XMLHttpRequest'
             },
-            body: convertDataToFormUrlEncoded(data),
+            body: JSON.stringify(data),
         });
 
         if (!response.ok) {
