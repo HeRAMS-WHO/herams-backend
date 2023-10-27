55376189a60ad812d7505b1c2c7b5c1a762bcf89
Rafael Pérez <rafaelbenitoperezgonzalez@gmail.com>
2023-10-27 13:12:49 +0200

    fix no-content answer in response 204

--- protected/react-src/src/services/httpMethods.js
+++ protected/react-src/src/services/httpMethods.js
@@ -57,5 +57,8 @@ const handleResponse = (response) => {
     if (!response.ok) {
         throw new Error("Network response was not ok");
     }
+    if (response.status === 204) {
+        return null;
+    }
     return response.json();
 };
