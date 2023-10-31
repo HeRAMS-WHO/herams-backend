0ff1bdba6ee19c9f317f98428af28ca119701f08
Rafael Pérez <rafaelbenitoperezgonzalez@gmail.com>
2023-10-31 09:07:50 +0100

    bugfix

--- protected/react-src/src/hooks/Role/useRoleList.js
+++ protected/react-src/src/hooks/Role/useRoleList.js
@@ -1,17 +1,17 @@
 import {fetchRoles, fetchRolesInProject, fetchRolesInWorkspace} from "../../services/apiProxyService";
 import {useEffect, useState} from "react";
 
-function useRoleList({projectId = null, workspaceId = null}) {
+function useRoleList({projectId = undefined, workspaceId = undefined} = {}) {
     const [rolesList, setRolesList] = useState([]);
     const refreshRolesList = async () => {
         let rolesResponse;
-        if (projectId !== null) {
+        if (projectId !== undefined) {
             rolesResponse = await fetchRolesInProject(projectId);
         }
-        if (workspaceId !== null) {
+        if (workspaceId !== undefined) {
             rolesResponse = await fetchRolesInWorkspace(workspaceId);
         }
-        if (projectId === null && workspaceId === null) {
+        if (projectId === undefined && workspaceId === undefined) {
             rolesResponse = await fetchRoles();
         }
         setRolesList(rolesResponse);
