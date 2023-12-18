696547eaaac7e11d6e42b63f9fbefc066a191550
ionutwho <ionut.borita@gmail.com>
2023-12-18 19:57:09 +0200

    added new table component: use material data grid instead of ag grid for user list

--- protected/react-src/src/components/Users/UserIndexTableHeader.jsx
+++ protected/react-src/src/components/Users/UserIndexTableHeader.jsx
@@ -1,88 +1,48 @@
 import {__} from "../../utils/translationsUtility";
-import DeleteButton from "../common/button/DeleteButton";
 import {UserIcon} from "../common/icon/IconsSet";
-
-const CustomLinkRenderer = (params) => {
-    const link = `/user/${params.data.id}/global-user-roles`;
-    return (
-        <a href={link} className={"agGridAnkur"}>{params.data.name}</a>
-    );
-};
+import {GridActionsCellItem} from "@mui/x-data-grid";
+import DeleteIcon from "@mui/icons-material/Delete";
+import dayjs from "dayjs";
 
 const UserIndexTableHeader = () => [
     {
         headerName: __('Id'),
         field: 'id',
-        checkboxSelection: false,
-        filter: true,
-        width: 100,
-        sortable: true,
+        type: 'number',
+        width: 50
     },
     {
         headerName: __('Name'),
-        checkboxSelection: false,
         field: 'name',
-        filter: true,
-        width: 200,
-        sortable: true,
-        valueGetter: (params) => params.data.name,
-        cellRenderer: CustomLinkRenderer,
-        comparator: (a, b) => a.localeCompare(b)
+        width: 200
     },
     {
         headerName: __('Email'),
         field: 'email',
-        checkboxSelection: false,
-        filter: true,
-        width: 200,
-        sortable: true,
-        comparator: (a, b) => a.localeCompare(b)
+        width: 200
     },
     {
         headerName: __('Created on'),
         field: 'created_date',
-        checkboxSelection: false,
-        filter: true,
-        width: 200,
-        sortable: true,
-        comparator: (a, b) => a.localeCompare(b),
+        type: 'date',
+        width: 150,
+        valueFormatter: (params) => params.value && dayjs(params.value).format('YYYY-MM-DD HH:MM')
     },
     {
         headerName: __('Last login'),
         field: 'last_login_date',
-        checkboxSelection: false,
-        filter: true,
-        width: 200,
-        sortable: true,
+        type: 'date',
+        width: 150,
+        valueFormatter: (params) => params.value && dayjs(params.value).format('YYYY-MM-DD HH:MM')
     },
     {
         headerName: __('Actions'),
         field: 'actions',
-        checkboxSelection: false,
-        filter: true,
-        cellRenderer: function (params) {
-            const confirmationText = __("Are you sure you want to delete the user's role?");
-            return (
-                <>
-                    <div className='d-flex'>
-                        <div>
-                            <button className="bg-transparent borderless cursor-pointer">
-                                <UserIcon title={__('Impersonate')}/>
-                            </button>
-                        </div>
-                        <div>
-                            <DeleteButton
-                                title={__("Delete User's Role")}
-                                html={confirmationText}
-                                confirmButtonText={__('Delete')}
-                                cancelButtonText={__('Cancel')}
-                            />
-                        </div>
-                    </div>
-                </>
-            )
-        },
-
+        type: 'actions',
+        getActions: (params) => [
+            <GridActionsCellItem icon={<UserIcon />} onClick={() => console.log('impersonate id ' + params.id)} label={__('Impersonate')} />,
+            <GridActionsCellItem icon={<DeleteIcon />} onClick={() => console.log('delete id ' + params.id)} label={__('Delete User\'s Role')} />,
+        ],
     }
 
 ];
