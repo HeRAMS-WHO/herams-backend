a0b4d09e96fb19d3a9dab9d289abdce223a68db6
ionutwho <ionut.borita@gmail.com>
2023-12-18 19:57:09 +0200

    added new table component: use material data grid instead of ag grid for user list
    (cherry picked from commit 696547eaaac7e11d6e42b63f9fbefc066a191550)

---
+++ protected/react-src/src/components/common/table/MuiTable.jsx
@@ -0,0 +1,44 @@
+import React, {useCallback, useMemo, useRef} from 'react';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+
+import {__} from "../../../utils/translationsUtility";
+import {Box} from "@mui/material"; // Optional theme CSS
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+
+const MuiTable = ({columnDefs, data}) => {
+    const gridRef = useRef();
+
+
+    // Example using Grid's API
+    const buttonListener = useCallback(e => {
+        gridRef.current.columnApi.resetColumnState();
+
+    }, []);
+
+    return (
+        <Grid>
+            {/*<div className="d-flex gap-1 mt-4 place-end">
+                <Button variant="contained" onClick={() => buttonListener()}>
+                    {__('Reset grid')}
+                </Button>
+            </div>*/}
+            <Box sx={{ height: 600, width: '100%' }}>
+                <DataGrid
+                    rows={data}
+                    columns={columnDefs}
+                    initialState={{
+                        pagination: {
+                            paginationModel: { page: 0, pageSize: 10 },
+                        },
+                    }}
+                    slots={{ toolbar: GridToolbar }}
+                    pageSizeOptions={[10, 20]}
+                    checkboxSelection
+                />
+            </Box>
+        </Grid>
+    );
+}
+
+export default MuiTable;
