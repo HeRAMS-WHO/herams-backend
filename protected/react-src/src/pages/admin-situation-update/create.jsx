e589a71f59df95b1d8f1bb01b0dd99b20d0270a0
Cristian Girlea <cristiangirlea@gmail.com>
2024-01-02 10:47:54 +0200

    fixing create and edit admin situatio not pointing correctly.

--- protected/react-src/src/pages/admin-situation-update/create.jsx
+++ protected/react-src/src/pages/admin-situation-update/create.jsx
@@ -1,7 +1,7 @@
 import SurveyWidget from "../../components/SurveyJs/SurveyWidget"
 const CreateAdminSituationUpdate = () => {
     const { hsduId } = params.value
-    const url = `${window.location.origin}/facility/${hsduId}/update-situation`
+    const url = `${window.location.origin}/facility/${hsduId}/create-admin-situation`
     return (<> <SurveyWidget url={url} /> </>)
 }
 
--- protected/react-src/src/pages/admin-situation-update/edit.jsx
+++ protected/react-src/src/pages/admin-situation-update/edit.jsx
@@ -1,7 +1,7 @@
 import SurveyWidget from "../../components/SurveyJs/SurveyWidget"
 const EditAdminSituationUpdate = () => {
-    const { hsduId } = params.value
-    const url = `${window.location.origin}/facility/${hsduId}/update-situation`
+    const { hsduId, updateId } = params.value
+    const url = `${window.location.origin}/facility/${hsduId}/edit-admin-situation/${updateId}`
     return (<> <SurveyWidget url={url} /> </>)
 }
 
