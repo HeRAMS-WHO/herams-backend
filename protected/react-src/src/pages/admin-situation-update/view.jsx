285127cb0b30490fd692713335ca9b063d29625e
Cristian Girlea <cristiangirlea@gmail.com>
2024-01-02 10:51:57 +0200

    fixing admin situation

--- protected/react-src/src/pages/admin-situation-update/view.jsx
+++ protected/react-src/src/pages/admin-situation-update/view.jsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react"
 import { get } from "../../services/httpMethods"
 import SurveyWidget from "../../components/SurveyJs/SurveyWidget"
 const ViewAdminSituationUpdate = () => {
-    const { hsduId } = params.value
-    const url = `${window.location.origin}/facility/${hsduId}/update-situation`
+    const { hsduId,updateId } = params.value
+    const url = `${window.location.origin}/facility/${hsduId}/view-admin-situation/${updateId}`
     return (<> <SurveyWidget url={url} /> </>)
 
 }
