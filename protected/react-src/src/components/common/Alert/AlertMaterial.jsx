7850063297f8ab9586c48f79c5e2c5608c534948
Cristian Girlea <cristiangirlea@gmail.com>
2024-01-30 10:14:32 +0200

    remove user button

---
+++ protected/react-src/src/components/common/Alert/AlertMaterial.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Dialog, DialogActions, DialogContent, DialogTitle, Slide, Alert, Button } from '@mui/material';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const AlertDialog = ({ open, handleClose, title, children }) => (
+  <Dialog
+    open={open}
+    TransitionComponent={Transition}
+    keepMounted
+    onClose={handleClose}
+    aria-describedby="alert-dialog-slide-description"
+  >
+    <DialogTitle>{title}</DialogTitle>
+    <DialogContent>{children}</DialogContent>
+    <DialogActions>
+      <Button onClick={handleClose}>Close</Button>
+    </DialogActions>
+  </Dialog>
+);
+
+const AlertMaterial = ({
+  type = 'success',
+  open,
+  handleClose,
+  showButton = false,
+  buttonName = '',
+  alertTitle = '',
+  alertMessage = ''
+}) => {
+
+  const defaultContents = {
+    success: {
+      title: "Success",
+      message: "This is a success alert — check it out!",
+      buttonName: "Show Success Alert"
+    },
+    warning: {
+      title: "Warning",
+      message: "This is a warning alert — check it out!",
+      buttonName: "Show Warning Alert"
+    },
+    error: {
+      title: "Error",
+      message: "This is an error alert — check it out!",
+      buttonName: "Show Error Alert"
+    }
+  };
+
+  const content = defaultContents[type];
+  const finalTitle = alertTitle || content.title;
+  const finalMessage = alertMessage || content.message;
+  const finalButtonName = buttonName || content.buttonName;
+
+  return (
+    <div>
+      {showButton && (
+        <Button onClick={handleClose}>{finalButtonName}</Button> // Changed to handleClose for consistency
+      )}
+
+      <AlertDialog open={open} handleClose={handleClose} title={finalTitle}>
+        <Alert severity={type}>{finalMessage}</Alert>
+      </AlertDialog>
+    </div>
+  );
+};
+
+export default AlertMaterial;
