24c7958d1e971b911b0625c958f3a410a310bf5d
Cristian Girlea <cristiangirlea@gmail.com>
2024-02-21 11:28:46 +0200

    csv export

---
+++ protected/react-src/src/components/common/Alert/SnackBarMaterial.jsx
@@ -0,0 +1,50 @@
+// CustomSnackbarWithProgress.jsx
+import React, { useEffect, useState } from 'react';
+import { Snackbar, Alert, LinearProgress } from '@mui/material';
+
+const CustomSnackbarWithProgress = ({ open, handleClose, message, severity, duration = 6000 }) => {
+    const [progress, setProgress] = useState(0);
+    const [internalOpen, setInternalOpen] = useState(false);
+
+    useEffect(() => {
+        if (open) {
+            setProgress(0); // Reset progress to 0 immediately
+            setInternalOpen(true);
+        } else {
+            setInternalOpen(false); // Close the snackbar immediately when open is set to false
+        }
+    }, [open]);
+
+    useEffect(() => {
+        // Only start the timer if the snackbar is internally open
+        if (internalOpen) {
+            const timer = setInterval(() => {
+                setProgress((prevProgress) => {
+                    const delta = 100 / (duration / 100); // Calculate progress increment
+                    const newProgress = prevProgress + delta;
+                    if (newProgress >= 100) {
+                        clearInterval(timer); // Stop the timer if progress is full
+                        return 100;
+                    }
+                    return newProgress;
+                });
+            }, 100); // Update progress every 100ms
+
+            return () => clearInterval(timer); // Cleanup timer on unmount
+        }
+    }, [internalOpen, duration]);
+
+    return (
+        <Snackbar open={internalOpen} autoHideDuration={duration} onClose={() => {
+            handleClose();
+            setInternalOpen(false); // Ensure internal state is reset on close
+        }}>
+            <Alert onClose={handleClose} severity={severity} sx={{ width: '100%' }} variant="filled">
+                {message}
+                <LinearProgress variant="determinate" value={progress} />
+            </Alert>
+        </Snackbar>
+    );
+};
+
+export default CustomSnackbarWithProgress;
--- protected/react-src/src/components/common/table/ExportOptions.jsx
+++ protected/react-src/src/components/common/table/ExportOptions.jsx
@@ -1,75 +1,97 @@
-import React, {useState} from 'react';
-import {IconButton, Menu, MenuItem} from '@mui/material';
+import React, { useState } from 'react';
+import {Box, CircularProgress, IconButton, Menu, MenuItem, Snackbar} from '@mui/material';
+import MuiAlert from '@mui/material/Alert';
 import SaveAltIcon from '@mui/icons-material/SaveAlt';
 import dayjs from "dayjs";
 import languageSelected from "../../../states/languageSelected";
+import CustomSnackbar from "../Alert/SnackBarMaterial";
 
-const ExportOptions = ({columnDefs, data}) => {
+const ExportOptions = ({ columnDefs, data }) => {
     const [anchorEl, setAnchorEl] = useState(null);
+    const [snackbarInfo, setSnackbarInfo] = useState({
+        open: false,
+        message: '',
+        severity: 'success',
+        duration: 3000
+    });
 
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
+    const handleClick = (event) => setAnchorEl(event.currentTarget);
+    const handleClose = () => setAnchorEl(null);
+    const handleSnackbarClose = () => setSnackbarInfo({ ...snackbarInfo, open: false });
+
+    const showSnackbar = (message, severity) => {
+        setSnackbarInfo({ open: true, message, severity, duration: 3000 });
     };
 
-    const handleClose = () => {
-        setAnchorEl(null);
+    // Separate function for formatting data
+    const formatData = (row, field) => {
+        let cellValue;
+        // Check if the field has i18n data
+        if (field === "i18n" && row[field] && row[field].title) {
+            const i18nValue = row[field].title;
+            // Fetch the value based on the selected language, fallback to 'en' if not available
+            cellValue = i18nValue[languageSelected.value] || i18nValue['en'];
+        } else {
+            // Handle non-i18n fields, including date formatting
+            cellValue = row[field];
+            if (columnDefs.find(colDef => colDef.field === field && colDef.type === 'date')) {
+                cellValue = cellValue ? dayjs(cellValue).format('YYYY-MM-DD') : '';
+            } else if (cellValue === undefined || typeof cellValue === 'object') {
+                cellValue = ''; // Exclude actions or handle other complex objects
+            }
+        }
+        // Escape quotes and wrap in quotes if necessary
+        return `"${String(cellValue).replace(/"/g, '""')}"`;
     };
 
-    const csvExport = () => {
-        const headers = columnDefs.map(colDef => colDef.headerName);
-        const fields = columnDefs.map(colDef => colDef.field);
+    const csvExport =  () => {
+        try {
+            const headers = columnDefs.map(colDef => colDef.headerName);
+            const fields = columnDefs.map(colDef => colDef.field);
 
-        // Prepare the CSV content
-        const csvContent = [
-            headers.join(','), // First line: Header names
-            fields.join(','), // Second line: Field names
-            // Following lines: Data rows
-            ...data.map(row =>
-                fields.map(field => {
-                    let cellValue;
-                    // Check if the field has i18n data
-                    if (field === "i18n" && row[field] && row[field].title) {
-                        const i18nValue = row[field].title;
-                        // Fetch the value based on the selected language, fallback to 'en' if not available
-                        cellValue = i18nValue[languageSelected.value] || i18nValue['en'];
-                    } else {
-                        // Handle non-i18n fields, including date formatting
-                        cellValue = row[field];
-                        if (columnDefs.find(colDef => colDef.field === field && colDef.type === 'date')) {
-                            cellValue = cellValue ? dayjs(cellValue).format('YYYY-MM-DD') : '';
-                        } else if (cellValue === undefined || typeof cellValue === 'object') {
-                            cellValue = ''; // Exclude actions or handle other complex objects
-                        }
-                    }
-                    // Escape quotes and wrap in quotes if necessary
-                    return `"${String(cellValue).replace(/"/g, '""')}"`;
-                }).join(',')
-            )
-        ].join('\r\n');
+            // Prepare the CSV content using the formatData function
+            const csvContent = [
+                headers.join(','), // First line: Header names
+                fields.join(','), // Second line: Field names
+                ...data.map(row => fields.map(field => formatData(row, field)).join(','))
+            ].join('\r\n');
 
-        // Create and trigger a download of the CSV file
-        const blob = new Blob([csvContent], {type: 'text/csv;charset=utf-8;'});
-        const url = URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.setAttribute('href', url);
-        link.setAttribute('download', 'exportedData.csv');
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+            // Create and trigger a download of the CSV file
+            const blob = new Blob([csvContent], {type: 'text/csv;charset=utf-8;'});
+            const url = URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.setAttribute('href', url);
+            link.setAttribute('download', 'exportedData.csv');
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+
+            // Success feedback
+            showSnackbar('Data exported successfully!', 'success');
+        } catch (error) {
+            // Error feedback
+            showSnackbar('Failed to export data.', 'error');
+        } finally {
+            handleClose();
+        }
     };
-
 
     return (
-        <div>
+        <Box position="relative" display="inline-block">
             <IconButton color="primary" size="large" onClick={handleClick}>
-                <SaveAltIcon/>
+                <SaveAltIcon />
                 <span>Export</span>
             </IconButton>
             <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
                 <MenuItem onClick={csvExport}>Export CSV</MenuItem>
-                {/* Add more menu items for other exports if needed */}
             </Menu>
-        </div>
+            <CustomSnackbar
+                open={snackbarInfo.open}
+                handleClose={handleSnackbarClose}
+                message={snackbarInfo.message}
+                severity={snackbarInfo.severity}
+            />
+        </Box>
     );
 };
 
