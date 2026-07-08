bb68fcae61e518e1c77a077ef6668aa9daa23cac
Ionut Borita <ionut.borita@gmail.com>
2026-07-08 23:49:58 +0300

    update business logic for completeness

--- protected/services/completeness/MaliResponseCompleteness.php
+++ protected/services/completeness/MaliResponseCompleteness.php
@@ -5,6 +5,12 @@ namespace prime\services\completeness;
 
 final class MaliResponseCompleteness extends GenericResponseCompleteness
 {
-
-
+    /**
+     * CHWs (MoSD3 = A11) have a reduced questionnaire without the CONDB and
+     * HFFUNCT questions; evaluation jumps from MoSD4 straight to HFACC.
+     */
+    protected function skipsBuildingConditionAndFunctionality(array $data): bool
+    {
+        return $this->scalar($data['MoSD3'] ?? null) === 'A11';
+    }
 }
--- protected/services/completeness/ResponseCompletenessInterface.php
+++ protected/services/completeness/ResponseCompletenessInterface.php
@@ -7,6 +7,7 @@ interface ResponseCompletenessInterface
 {
     /**
      * @param array $data the raw response data (contents of the `data` JSON column)
+     * @param string|null $date the response `date` column (stored outside the JSON data)
      */
-    public function isComplete(array $data): bool;
+    public function isComplete(array $data, ?string $date): bool;
 }
